68a760b3f9f86c8780c5a10a645d441377c056fc
Allison-Turner <hermione2259@gmail.com>
2020-07-03 14:11:16 -0400

    pushing forward with node parsing, some README stuff

--- build_map.py
+++ build_map.py
@@ -2,7 +2,9 @@ from argparse import ArgumentParser
 from net_topology import Topology, Node, Alias, Link
 import subprocess, datetime, re
 
-timestamp = datetime.date.now(datetime.timezone.utc).strftime("%H-%M-%S-%d-%m-%Y")
+from time import strftime,localtime
+timestamp = strftime("%H-%M-%S-%d-%m-%Y", localtime())
+# timestamp = datetime.datetime.now(datetime.timezone.utc).strftime("%H-%M-%S-%d-%m-%Y")
 
 # IPv4 via midar-iffinder: .nodes + .links + .ifaces + .as + .geo
 # IPv6 via speedtrap: .nodes + .links + .as + .geo
@@ -21,7 +23,7 @@ parser.add_argument("-x", "--extract_files", dest="extract_files", default=False
 parser.add_argument("-w", "--download_files", dest="download_files", default=False, help="Whether to download the data files from CAIDA's data server")
 
 def download_files(extension, location, day, month, year):
-    download_log = open("download" + timestamp + ".log", "a")
+    download_log = open(location + "download" + timestamp + ".log", "a")
 
     # Download all files to listed folder location
     for file in file_types:
@@ -53,7 +55,7 @@ def download_files(extension, location, day, month, year):
 
 def decompress(extension, location):
     # Prep log file
-    decompress_log = open("decompression"+ timestamp + ".log", "a")
+    decompress_log = open(location + "decompression"+ timestamp + ".log", "a")
 
     #decompress archive files
     if extension == ".bz2":
@@ -96,34 +98,37 @@ def decompress(extension, location):
 
 
 def main():
-    # gives us:
-    # args.folder_loc [ $HOME/location/of/ITDK/data/files/ ]
-    # args.compression_ext [ .<file extension> string ]
-    # args.extract_files [ boolean ]
     args = parser.parse_args()
 
     # Initialize data structures
-    ipv4_net = Net_Topo()
-    ipv6_net = Net_Topo()
+    ipv4_net = Topology()
+    ipv6_net = Topology()
 
     # Download data archives
     if args.download_files:
+        print("Downloading files from CAIDA data server")
         download_files(args.compression_ext, args.folder_loc, args.day, args.month, args.year)
 
     #Extract files
     if args.extract_files or args.download_files:
+        print("Decompressing files")
         decompress(args.compression_ext, args.folder_loc)
 
     # Open IPv4 nodes file to begin extracting node objects
     ipv4_nodes = open(args.folder_loc + ipv4_topo_choice + file_types[0], "r")
 
+    found = 0
+
+    print("Parsing .nodes file to add vertices")
     for line in ipv4_nodes:
         # read in node objects
         prefix = re.search("\Anode N([0-9]+)", line)
         if prefix != None:
+            # print("Current line: " + line)
+
             # Parse node ID
             id_start = prefix.group().index("N")
-            n_ID = prefix.group()[id_start:]
+            n_ID = prefix.group()[(id_start + 1):]
             node = Node(n_ID)
 
             # Split entry by whitespace
@@ -134,13 +139,27 @@ def main():
                 containLetters = re.findall("[a-zA-Z]", token)
                 if len(containLetters) < 1 and len(token) > 0:
                     node.add_alias(token)
+                    # print("New alias for node " + n_ID + " at " + token)
 
             ipv4_net.add_node(node)
-        else:
-            print(line)
+
+            print(node.node_id + " added")
+            found += 1
+            if found > 100:
+                break
 
     ipv4_nodes.close()
 
+    # print("Found " + len(ipv4_net.vertices) + " nodes.")
+    for n in ipv4_net.vertices:
+        print("Node " + n.node_id + " has " + str(len(n.aliases)) + " aliases.")
+        print("\n")
 
+    # ipv4_ifaces = open(args.folder_loc + ipv4_topo_choice + file_types[4], "r")
+
+    # for line in ipv4_ifaces:
+        # get iface entries
+
+    # ipv4_ifaces.close()
 
 main()
--- net_topology.py
+++ net_topology.py
@@ -1,31 +1,41 @@
 class Topology:
-    vertices = []
-    edges = []
 
     def __init__(self, is_ipv4=True):
         self.ipv4 = is_ipv4
+        self.vertices = []
+        self.edges = []
 
     # Add a new node to the vertices list, keeping the list ordered by node ID
     def add_node(self, node):
-        # fill in later
-        print("vfehvf")
+        if len(self.vertices) == 0:
+            self.vertices.append(node)
+        else:
+            index = 0
+            inserted = False
+            while index < len(self.vertices) and not inserted:
+                if node.node_id > self.vertices[index].node_id:
+                    self.vertices.insert(index, node)
+                    inserted = True
+
+                index += 1
+
+            if not(inserted):
+                self.vertices.append(node)
 
     # Add a new link to the edges list, keeping the list ordered by link ID
     def add_link(self, link):
-        # fill in later
-        print("affdrdvs")
+        edges.append(link)
+        edges.sort(key=link_sort)
 
 class Node:
-    aliases = []
-    links = []
-    as_owner = -1
-
-    def __init__(self, nID=-1):
-        aliases.append(Alias(addr))
-        self.nodeID = nID
+    def __init__(self, nID=-1, as=-1):
+        self.node_id = nID
+        self.aliases = []
+        self.links = []
+        self.as_owner = as
 
     def add_alias(self, alias):
-        if self.nodeID != -1:
+        if self.node_id != -1:
             self.aliases.append(Alias(alias))
         else:
             print("Cannot add an alias to a node without a node ID")
@@ -38,18 +48,17 @@ class Node:
         self.as_owner = ASN
 
 class Alias:
-    links = []
-
     def __init__(self, addr, T=False, D=False):
+        self.links = []
         self.address = addr
         self.transitHop = T
         self.destHop = D
 
-    def add_link(self, linkID):
+    def add_link(self, link_id):
         print("add new link to node interface, ordering list by link ID")
 
 class Link:
     def __init__(self, lID, n1, n2):
-        self.linkID = lID
+        self.link_id = lID
         self.node1 = n1
         self.node2 = n2
